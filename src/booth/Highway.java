32855a2b1c2d132bf07e44f9dd29564f1d99e9d4
SilversKnit <rahultokill@gmail.com>
2023-01-30 15:16:14 +0530

    frst commit

---
+++ src/booth/Highway.java
@@ -0,0 +1,16 @@
+package booth;
+
+public class Highway {
+	
+	public Highway() {
+		System.out.println("1");
+		
+	}
+    
+	
+	public static void main(String[] args) {
+		Highway h = new Highway();
+		//h.cars();
+		System.out.println("2");
+	}
+}
---
+++ src/booth/Stateway.java
@@ -0,0 +1,17 @@
+package booth;
+
+public class Stateway extends Highway {
+	
+	 public  Stateway() {
+		super();
+		
+	}
+	
+	public static void main(String[] args) {
+		Stateway s = new Stateway();
+		//s.bike();
+		
+		System.out.println("3");
+	}
+
+}
